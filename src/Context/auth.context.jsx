d6b2d6d814f768c42c06bb135bd93688224fd46a
Joao Borrega <joaoborrega18@gmail.com>
2023-09-08 10:10:12 +0100

    final one

--- src/Context/auth.context.jsx
+++ src/Context/auth.context.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect, createContext } from "react";
 import axios from "axios";
 
-/* const API_URL = "https://dog-whisperer.onrender.com"; */
-const API_URL = "http://localhost:5005";
+const API_URL = "https://dog-whisperer.onrender.com";
 
 const AuthContext = createContext();
 
